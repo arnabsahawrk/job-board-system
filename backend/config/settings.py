5c108305b4f693931c5038fe1cadb727b07a20d2
Arnab Saha <arnabsahawrk@gmail.com>
2026-02-22 20:59:13 +0600

    feat: add CORS support

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "cloudinary",
     "anymail",
+    "corsheaders",
     "apps.core",
     "apps.applications",
     "apps.authentication",
@@ -45,12 +46,20 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://arnabsahawrk-jobly.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "config.urls"
 
